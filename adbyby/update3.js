6270157527f1987a8138a46d7ef315f58f116cc6
tekintian <tekintian@gmail.com>
2025-12-10 09:12:26 +0800

    update

--- adbyby/update3.js
+++ adbyby/update3.js
@@ -1,6 +1,3 @@
-;更新规则url,dir,size,reverse,reverse
-https://gitee.com/tekintian/adt-rules/raw/master/adbyby/lazy.txt	\data\lazy.txt	559336	4	0	
-https://gitee.com/tekintian/adt-rules/raw/master/adbyby/video.txt	\data\video.txt	 1128	4	0	
-!http://update.adbyby.com/rule3/user.action	\user.action	4	5	0	
-
-
+;update-rules-url,dir,size,reverse,reverse
+https://gitee.com/tekintian/adt-rules/raw/master/adbyby/lazy.txt	\data\lazy.txt	559336	4	0
+https://gitee.com/tekintian/adt-rules/raw/master/adbyby/video.txt	\data\video.txt	 1128	4	0
